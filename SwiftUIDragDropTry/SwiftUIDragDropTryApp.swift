eaad9cb64a924ce515df3a84b32d840dd70f6485
devproo <ipeerless0@gmail.com>
2023-08-23 16:53:40 +0300

    Initial Commit

---
+++ SwiftUIDragDropTry/SwiftUIDragDropTryApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIDragDropTryApp.swift
+//  SwiftUIDragDropTry
+//
+//  Created by ipeerless on 23/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIDragDropTryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
